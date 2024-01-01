f9f93813c9de9070a79549e5f2deadb154277678
rabinacharya11 <charms2074@gmail.com>
2024-01-01 14:49:08 +0545

    build : explore and create post

--- lib/widgets/homescreen/thought_card.dart
+++ lib/widgets/homescreen/thought_card.dart
@@ -4,35 +4,45 @@ import 'package:quotes_app/core/theme/custom_text_style.dart';
 class ThoughtCard extends StatelessWidget {
   final String quote;
   final String author;
+  final double? height;
   const ThoughtCard({
     super.key,
     required this.quote,
     required this.author,
+    this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 401,
-      height: 306,
+      height: height ?? 306,
       decoration: ShapeDecoration(
         color: const Color(0xFFF5C5C5),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
       child: Center(
         child: FittedBox(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                quote,
-                style: AppTextStyle.quoteHeading,
-              ),
-              Text(
-                author,
-                style: AppTextStyle.bodyTextSmall,
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 380,
+                  child: Text(quote,
+                      textAlign: TextAlign.center,
+                      style: AppTextStyle.quoteHeading.copyWith(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      )),
+                ),
+                Text(
+                  author,
+                  style: AppTextStyle.bodyTextSmall,
+                ),
+              ],
+            ),
           ),
         ),
       ),
